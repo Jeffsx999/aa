a918632f03127dc5e7e2efd9bd155d693da497df
Jefferson lima <113362045+Jeffsx999@users.noreply.github.com>
2022-09-26 15:00:54 -0300

    Add files via upload

---
+++ pop-up.py
@@ -0,0 +1,10 @@
+# janela = Tk()
+# janela.title ='Pytube video downloader'
+
+# Label(janela, text='Digite o metodo').grid(row=0)
+# e1 = Entry(janela)
+# e2 = Entry(janela)
+# e1.grid(row=0, column=1)
+# e2.grid(row=1, column=1)
+
+# janela.mainloop()
